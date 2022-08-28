d6b51678459db2e8f145e92db23d38756ccf214a
Alina Raimova <vifichik@gmail.com>
2022-08-28 22:44:11 +0300

    Display welcome-page, jokes, memes and gifs without design

--- main.py
+++ main.py
@@ -3,24 +3,37 @@ import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+api_key = "74326d539da14302931e141df4fc64f7"
 
 @app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/jokes")
 def jokes():
-    api_key = "74326d539da14302931e141df4fc64f7"
-    # param = jokes
     r_jokes = requests.get(f"https://api.humorapi.com/jokes/random?api-key={api_key}")
-    r_memes = requests.get(f"https://api.humorapi.com/memes/random?api-key={api_key}")
-    r_gifs = requests.get(f"https://api.humorapi.com/gif/search?query=dog&number=3&api-key={api_key}")
-    # print(jokes.text)
     data_jokes = json.loads(r_jokes.text)
-    data_memes = json.loads(r_memes.text)
-    data_gifs = json.loads(r_gifs.text)
-
     joke = data_jokes['joke']
+    return render_template('jokes.html', joke=joke)
+
+
+@app.route("/memes", methods=('GET',))
+def memes():
+    r_memes = requests.get(f"https://api.humorapi.com/memes/random?api-key={api_key}")
+    data_memes = json.loads(r_memes.text)
     meme = data_memes['url']
-    gif = data_gifs['images']['url']
+    return render_template('memes.html', meme=meme)
 
-    return render_template('index.html', joke=joke, meme=meme, gif=gif)
 
-app.run(debug=True)
+@app.route("/gif")
+def gifs():
+    r_gifs = requests.get(f"https://api.humorapi.com/gif/search?query=dog&number=3&api-key={api_key}")
+    data_gifs = json.loads(r_gifs.text)
+    gif = data_gifs['images'][0]['url']
+    return render_template('gif.html', gif=gif)
+
+
+# app.run(debug=True)
+if __name__ == '__main__':
+    # Run the app server on localhost:4449
+    app.run('localhost', 5000)
